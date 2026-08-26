5c2083705453bfe121984b2b96857df5d8d29a16
mangono1 <dikk418@gmail.com>
2026-08-26 14:34:54 +0700

    fix: sync CPUTorch Python version to 0.5.4

--- python/cputorch/__init__.py
+++ python/cputorch/__init__.py
@@ -4,12 +4,12 @@ CPUTorch
 
 CPU-focused tensor and neural-network engine written in C++.
 
-Version 0.5.3.
+Version 0.5.4.
 """
 
 from ._core import Tensor
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 
 __all__ = [
     "Tensor",
